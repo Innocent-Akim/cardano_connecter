298bab3a3c8acfcb2f296a3f1303fe0f6466a833
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-16 12:31:13 +0200

    feat: close popup onclick on navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,19 @@ import { NavLink } from "react-router-dom";
 import { AiFillInstagram } from "react-icons/ai";
 import { BsFacebook, BsTwitter, BsLinkedin, } from "react-icons/bs";
 
+import { useStateContext } from "../context/contextProvider";
+
 const NavBar = () => {
+    const { setCallPopup } = useStateContext()
 
     const classLink = 'text-black hover:decoration-white hover:text-blue-800 font-semibold'
     const classMediasSociaux = 'text-blue-900 text-lg hover:text-blue-800 hover:animate-pulse font-semibold'
 
     return (
-        <nav className='flex justify-between items-center fixed top-0 right-0 left-0 z-50 bg-gray-50 w-full h-14 mb-10 px-7 shadow-lg'>
+        <nav
+            className='flex justify-between items-center fixed top-0 right-0 left-0 z-50 bg-gray-50 w-full h-14 mb-10 px-7 shadow-lg'
+            onClick={() => setCallPopup(false)}
+        >
             <div>
                 <img src='' alt='' />
             </div>
