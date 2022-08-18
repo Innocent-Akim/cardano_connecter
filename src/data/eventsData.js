772cc7c1674d2be2fd091bf921cf74ea1caf7004
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-18 14:30:51 +0200

    adding events data

---
+++ src/data/eventsData.js
@@ -0,0 +1,44 @@
+export const eventsData = [
+    {
+        date: "24-07-2022",
+        objectif: "Exposer le desaccord sur la hausse de carburant",
+        modalites: "Une marche pacifique en partant de l'entree du president jusqu'a la frontiere",
+        lieu: "Terminus",
+        etat: "next"
+    },
+    {
+        date: "24-07-2022",
+        objectif: "Exposer le desaccord sur la hausse de carburant",
+        modalites: "Une marche pacifique en partant de l'entree du president jusqu'a la frontiere",
+        lieu: "Terminus",
+        etat: "next"
+    },
+    {
+        date: "24-07-2022",
+        objectif: "Exposer le desaccord sur la hausse de carburant",
+        modalites: "Une marche pacifique en partant de l'entree du president jusqu'a la frontiere",
+        lieu: "Terminus",
+        etat: "recent"
+    },
+    {
+        date: "24-07-2022",
+        objectif: "Exposer le desaccord sur la hausse de carburant",
+        modalites: "Une marche pacifique en partant de l'entree du president jusqu'a la frontiere",
+        lieu: "Terminus",
+        etat: "next"
+    },
+    {
+        date: "24-07-2022",
+        objectif: "Exposer le desaccord sur la hausse de carburant",
+        modalites: "Une marche pacifique en partant de l'entree du president jusqu'a la frontiere",
+        lieu: "Terminus",
+        etat: "recent"
+    },
+    {
+        date: "24-07-2022",
+        objectif: "Exposer le desaccord sur la hausse de carburant",
+        modalites: "Une marche pacifique en partant de l'entree du president jusqu'a la frontiere",
+        lieu: "Terminus",
+        etat: "recent"
+    },
+]
