b24d67eac0b17209ad3c5ec9af0d17a7afdace70
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-19 15:23:29 +0200

    style: finalisation leadership section

--- src/components/home/CardLeader.jsx
+++ src/components/home/CardLeader.jsx
@@ -4,12 +4,12 @@ import { BsLinkedin, BsTwitter } from 'react-icons/bs';
 const CardLeader = ({ srcImage, nom, fonction, linkedin, twitter }) => {
     return (
         <div className='flex flex-col items-center justify-center bg-white p-4 box-border w-56 rounded-2xl border-1 hover:bg-gray-100 hover:shadow-2xl hover:animate-pulse'>
-            <img src={srcImage} alt="" className='rounded-full w-72' />
+            <img src={srcImage} alt="" className='rounded-full h-32 w-32 bg-cover mb-9' />
             <h1 className='text-xl font-medium mb-4'>{nom}</h1>
-            <p className='text-center font-light mx-2'>{fonction}</p>
-            <div className='flex justify-between'>
-                <a href={linkedin}><BsLinkedin /></a>
-                <a href={twitter}><BsTwitter /></a>
+            <p className='text-center font-semibold mx-2'>{fonction}</p>
+            <div className='flex justify-between text-2xl'>
+                <a href={linkedin} target='_blank' rel="noreferrer"><BsLinkedin className='m-3' /></a>
+                <a href={twitter} target='_blank' rel="noreferrer"><BsTwitter className='m-3' /></a>
             </div>
         </div>
     )
