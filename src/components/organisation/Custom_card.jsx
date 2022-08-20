7ff99223d9910b5841905ce47bec67931d0acfb3
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-20 07:39:12 +0200

    style: design organisations page

--- src/components/organisation/Custom_card.jsx
+++ src/components/organisation/Custom_card.jsx
@@ -9,12 +9,14 @@ import { AiFillLinkedin } from 'react-icons/ai'
 
 const Custom_card = ({ name, image }) => {
   return (
-    <div className='bg-fixed translate mt-2 h-75 w-200  bg-gray-50 hover:translate-y-0'>
+    <div className='bg-fixed mt-2 h-75 w-200 bg-gray-50 shadow-lg p-4'>
       <p className='text-18 font-bold p-2'><span className='text-18 text-blue-900 font-extrabold'>{name}</span></p>
       <div className=''></div>
       <div className='flex m-2 p-2 bg-white'>
-        <p className='text-justify text-14 font-normal'>Lorem ipsum dolor. Aspernatur quo dolorem porro, nostrum libero dignissimos minima explicabo ipsam illo beatae in magnam adipisci error ad quas iste fugit et saepe?</p>
-        <img className='w-52 h-44 pl-2 bg-left-top hover:translate-y-0 cursor-pointer' src={image}></img>
+        <p className='text-justify text-14 font-light'>
+          Lorem ipsum dolor. Aspernatur quo dolorem porro, nostrum libero dignissimos minima explicabo ipsam illo beatae in magnam adipisci error ad quas iste fugit et saepe?
+        </p>
+        <img className='w-52 h-44 pl-2  cursor-pointer' src={image}></img>
       </div>
 
       <div className='bottom-0  h-10 w-200 bg-gray-50 '>
--- src/pages/Organisation.jsx
+++ src/pages/Organisation.jsx
@@ -1,28 +1,26 @@
 
 /* eslint-disable react/jsx-pascal-case */
-import React, { Component } from 'react'
+import React from 'react'
 import Custom_card from '../components/organisation/Custom_card';
 import { initDataOrga } from '../data/orgaData'
 
-export class Organisation extends Component {
-    render() {
-        return (
-            <>
-                <div className='overscroll-auto hover:overscroll-contain m-16 mx-4 flex flex-col justify-center items-center bg-gray-50'>
-                    <div className='grid grid-cols-3 gap-2 w-screen h-screen bg-transparent'>
-                        {initDataOrga.map((data, index) =>
-                            <div key={index}>
-                                <Custom_card name={data.name} image={data.image} />
-                            </div>
+const Organisation = () => {
 
-                        )}
+    return (
+        <div className='mx-7'>
+            <div className='overscroll-auto hover:overscroll-contain m-16 mx-4 flex flex-col justify-center items-center bg-gray-50'>
+                <div className='grid grid-cols-3 gap-3 bg-transparent'>
+                    {initDataOrga.map((data, index) =>
+                        <div key={index}>
+                            <Custom_card name={data.name} image={data.image} />
+                        </div>
 
-                    </div>
+                    )}
                 </div>
-            </>
+            </div>
+        </div>
 
-        )
-    }
+    )
 }
 
 export default Organisation
