e7d51acf12569fbbbef07c6bbc01e06fc5696ddc
GLOIRE MUTALIKO <mufunyig@gmail.com>
2023-02-18 01:19:44 +0200

    about content

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -6,9 +6,10 @@ const About = () => {
     return (
         <div className='grid grid-cols-1 md:flex mt-80 md:mt-20 w-full '>
             <div className='flex flex-col w-full md:w-1/2 md:pr-20 '>
-                <h1 className='text-2xl md:text-4xl font-semibold mb-5'>Qu'est-ce que nous faison ?</h1>
+                <h1 className='text-2xl md:text-4xl font-semibold mb-5'>Qu'est-ce que nous faisons ?</h1>
                 <p className='font-light text-sm md:text-base text-justify'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quas quidem similique atque quisquam repellat quod, eaque pariatur ut, animi ab et iste. Animi, voluptatum nesciunt iste corrupti blanditiis qui impedit ipsum a, obcaecati sequi nihil consectetur quae quisquam quaerat distinctio fugit, cupiditate quo in atque voluptate magni! Atque, excepturi?
+                    Par l’application Shiriki-RDC, des structures et plateformes donnent généralement des informations sur la cause, des actions à entreprendre et des moyens d'impliquer le plus grand nombre de personnes. Elle peut également fournir des outils pour faciliter l'organisation d'activités et l'engagement des participants.
+                    Shiriki-RDC étant une application basée sur la technologie blockchain, permet de collecter des fonds partout dans le monde entier pour soutenir une cause dans n’importe quelle entité (ville, village, faubourg, campagne, …) en éliminant des intermédiaires inutiles. C’est ainsi que certains portefeuilles dits wallets y sont intégrés.
                 </p>
                 <img src={coding} alt='' />
             </div>
